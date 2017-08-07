2681beb8270bd4858c5f86f78b28665261d93cde
karina palacios <karina.spalacios@gmail.com>
2017-08-07 13:28:30 -0400

    añadiendo trabajo

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+
+
+window.addEventListener('scroll', function () {
+  const distanciaVertical = window.pageYOffset || document.documentElement.scrollTop,
+  header = document.getElementById('header');
+  
+  if (distanciaVertical > 400) {
+    header.classList.add("navbar-color");
+  } else {
+    header.classList.remove("navbar-color");
+  }
+});

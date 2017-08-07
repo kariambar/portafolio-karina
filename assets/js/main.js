047ebedd0b5c42ecdc15c9584cb3a240ef4ad959
karina palacios <karina.spalacios@gmail.com>
2017-08-07 15:35:44 -0400

    arreglando responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,12 @@
 
-
-
 window.addEventListener('scroll', function () {
   const distanciaVertical = window.pageYOffset || document.documentElement.scrollTop,
   header = document.getElementById('header');
   
-  if (distanciaVertical > 400) {
+  if (distanciaVertical > 500) {
     header.classList.add("navbar-color");
   } else {
     header.classList.remove("navbar-color");
   }
 });
+
